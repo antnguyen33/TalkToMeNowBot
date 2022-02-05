16c742b01b7caf4ea76b16aecbf9dc9966997d77
btlee264855 <btlee264855@gmail.com>
2022-02-05 14:42:48 -0500

    Revert "Revert "removed poop, added help""
    This reverts commit a8aa9b89439cf3d4b7655af1c136cafaa1e2a2d2.

--- src/main/java/com/github/antnguyen33/hackvioletbot/Main.java
+++ src/main/java/com/github/antnguyen33/hackvioletbot/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Insert your bot's token here
-        String token = "placeholdertoken";
+        String token = "OTM5NTM4MDkzNjY3NjYzOTUy.Yf6TCQ._VtH3hL6wIFErnTiOhoXst8npOY";
 
         DiscordApi api = new DiscordApiBuilder().setToken(token).login().join();
 
@@ -21,6 +21,21 @@ public class Main {
                     + " - Cook Counseling Center: 540-231-6557\r\n"
                     + " - Dean of Students Office: 540-231-3787\r\n"
                     + " - Virginia Tech Police Department: 540-382-4343! ``` ");
+            }
+        });
+        //Add a listener which answer with possible input calls when the user types !help, should be public  
+        api.addMessageCreateListener(event -> {
+            if (event.getMessageContent().equalsIgnoreCase("!help")) {
+                event.getChannel().sendMessage(
+                    " ```Welcome to the TalkToMeNow Bot,a responsive support chat bot \n"
+                    +"to interact with me, you can use !talk for a more tailored response \n"
+                    +"or I can provide you with a list of general resources if you need help more urgently using !vtresources```");
+            }
+        });
+        api.addMessageCreateListener(event -> {
+            boolean poo = true;
+            if (poo == true) {
+                event.getChannel().sendMessage("poop");
             }
         });
 
